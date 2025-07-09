12b1b3858cbf7dcd8c55f4540f0097892458b02b
Hasna Salihi <salihihasna02@gmail.com>
2025-07-09 09:49:26 +0100

    T

--- Week3/Day3/Challenge/EX-letter.js
+++ Week3/Day3/Challenge/EX-letter.js
@@ -2,13 +2,12 @@ const textInput = document.getElementById("textInput");
 const errorMessage = document.getElementById("errorMessage");
 
 textInput.addEventListener("input", function (e) {
-  // Get current value
+
   let value = this.value;
 
-  // Remove any non-letter characters using regex
   const newValue = value.replace(/[^a-zA-Z]/g, "");
 
-  // If we removed anything, show error and update value
+ 
   if (newValue !== value) {
     errorMessage.textContent = "Only letters are allowed!";
     this.value = newValue;
