e6ed6e3bce29287c9714af4eff56028c56e4549f
Hasna Salihi <salihihasna02@gmail.com>
2025-07-17 17:42:20 +0100

    Robot-Friends

---
+++ Week4/Day5/Robo-Friends Search/Robo-Friends.js
@@ -0,0 +1,106 @@
+const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    image: "https://robohash.org/1?200x200",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    image: "https://robohash.org/2?200x200",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    image: "https://robohash.org/3?200x200",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    image: "https://robohash.org/4?200x200",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    image: "https://robohash.org/5?200x200",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    image: "https://robohash.org/6?200x200",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    image: "https://robohash.org/7?200x200",
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    image: "https://robohash.org/8?200x200",
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    image: "https://robohash.org/9?200x200",
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    image: "https://robohash.org/10?200x200",
+  },
+];
+
+function display(items) {
+    const grid = document.getElementById("grid");
+    grid.innerHTML= ''
+  for (let item of items) {
+    
+    const card = document.createElement("div");
+    const image = document.createElement("img");
+    image.src = item.image;
+    const name = document.createElement("h6");
+    name.textContent = item.name;
+    const email = document.createElement("p");
+    email.textContent = item.email;
+
+    card.appendChild(image);
+    card.appendChild(name);
+    card.appendChild(email);
+    grid.appendChild(card);
+
+    card.classList.add("card");
+  }
+}
+display(robots)
+const search = document.getElementById("search");
+search.addEventListener("input", (e) => {
+    const searchValue = e.target.value.toLowerCase()
+    
+  
+
+  const filtred = robots.filter((robot) => robot.name.toLowerCase().includes(searchValue));
+  display(filtred);
+})
+
+
